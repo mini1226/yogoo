a5848c2b9a24000d47fabed8d496df7a440f9d6d
miniduiit <minidu.2019321@iit.ac.lk>
2023-04-12 21:18:52 +0530

    first page ui

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'', redirectTo: 'start', pathMatch: "full"},
+  {path:'start', loadChildren: () => import('./features/layout/main/main.module').then(m => m.MainModule)}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+
+// const routes: Routes = [
+//   {path: '', redirectTo: 'sign-in', pathMatch: 'full'},
+//   {path: 'sign-in', loadChildren: () => import('./module/auth/auth.module').then(m => m.AuthModule)},
+//   {path: 'main', loadChildren: () => import('./module/page/page.module').then(m => m.PageModule), canActivate: [AuthGuard]},
+// ];
